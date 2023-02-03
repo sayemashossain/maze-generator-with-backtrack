cd55c519258f9e5b4751e96ad1820c08c61d39ab
sayemashossain <sayemashossain@gmail.com>
2023-02-04 00:54:48 +1100

    Maze grid created, current cell iterates to neighbours

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let canHeight;
 let canWidth;
 
@@ -9,11 +7,14 @@ const rows = 10;
 
 let grid = []
 
+let current;
+
 
 function setup() {
     canWidth = side * cols;
     canHeight = side * rows;
     createCanvas(canWidth, canHeight)
+    frameRate(5)
 
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x < rows; x++) {
@@ -21,14 +22,28 @@ function setup() {
             grid.push(cell)
         }
     }
-
+    current = grid[0]
 }
 
 function draw() {
     background(51);
     for (let index = 0; index < grid.length; index++) {
         grid[index].show()
+        grid[index].checkNeighbours()
     }
+    current.visited = true;
+    const next = current.checkNeighbours();
+    if (next != undefined) {
+        next.visited = true;
+        current = next;
+    }
+
+    noLoop();
+}
+
+function getIndex(i, j) {
+    if (i < 0 || j < 0 || i >= rows || j >= cols) { return -1 }
+    return i + j * cols;
 }
 
 function Cell(i, j) {
@@ -36,12 +51,30 @@ function Cell(i, j) {
     this.j = j;
     this.walls = {
         tp: true,
-        lt: true,
-        bm: true,
         rt: true,
+        bm: true,
+        lt: true,
+    }
+    this.visited = false;
+    this.checkNeighbours = () => {
+        const unvisitedNeighbours = [];
+        const allNeighbours =
+        {
+            t: grid[getIndex(i, j - 1)],
+            r: grid[getIndex(i + 1, j)],
+            b: grid[getIndex(i, j + 1)],
+            l: grid[getIndex(i - 1, j)]
+        }
+
+        for (const [key, value] of Object.entries(allNeighbours)) {
+            if (value != undefined && value.visited == false) unvisitedNeighbours.push(value)
+        }
+
+        if (unvisitedNeighbours.length > 0) return unvisitedNeighbours[floor(random(0, unvisitedNeighbours.length))]
+        else return undefined
     }
 
-    this.show = function () {
+    this.show = () => {
         const x = this.i * side;
         const y = this.j * side;
 
@@ -51,6 +84,11 @@ function Cell(i, j) {
         if (this.walls.rt === true) line(x + side, y, x + side, y + side)
         if (this.walls.bm === true) line(x + side, y + side, x, y + side)
         if (this.walls.lt === true) line(x, y + side, x, y)
+
+        if (this.visited) {
+            fill(0, 255, 255, 100)
+            rect(x, y, side, side)
+        }
 
     }
 }
